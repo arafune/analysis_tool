9736275c9df7e990594c465b9371c4f152e86fb3
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2016-11-24 12:43:59 +0900

    rhksm4 module for loading .sm4 file

--- rhk/rhk/rhksm4.py
+++ rhk/rhk/rhksm4.py
@@ -8,6 +8,7 @@ For writing the code I used some ideas from the Gwyddion project
 are followed from it.
 '''
 
+import io
 import struct
 from types import MethodType
 
@@ -32,61 +33,281 @@ class RHKObject:
     packer = ExtStruct('<3I')
 
     classes = {}
+
     @classmethod
     def registObjType(self, obj_id, obj_name):
         RHKObject.classes[obj_id] = obj_name
 
+    objectIds = ['Undefined', 'PageIndexHeader', 'PageIndexArray',  # 0 1 2
+                 'PageHeader', 'PageData', 'ImageDriftHeader',      # 3 4 5
+                 'ImageDrift', 'SpecDriftHeader', 'SpecDriftData',  # 6 7 8
+                 'ColorInfo', 'StringData', 'TipTrackHeader',       # 9 10 11
+                 'TipTrackData', 'PRM', 'Thumbnail',                # 12 13 14
+                 'PRMHeader', 'ThumbnailHeader', 'AppInfo']         # 15 16 17
 
     def __init__(self, fhandle, parent):
         self.parent = parent
         self.objtype, self.offset, self.size = \
-                        RHKObject.packer.unpack_from_file(fhandle)
+            RHKObject.packer.unpack_from_file(fhandle)
+        self.objname = ""
         self.children = []
         if self.objtype in RHKObject.classes:
+            self.objname = RHKObject.objectIds[self.objtype]
             objclass = RHKObject.classes[self.objtype]
             if hasattr(objclass, 'read'):
                 self.read = MethodType(objclass.read, self)
             if hasattr(objclass, '__str__'):
                 self.__str__ = MethodType(objclass.__str__, self)
+#
+
+    def __str__(self):
+        if self.objtype in RHKObject.classes:
+            return RHKObject.classes[self.objtype].__str__(self)
+
+        this = 'RHKObject of type {0.objtype} @ {0.offset} x {0.size}'.\
+               format(self)
+        if self.children:
+            return this + "\n" + "\n".join(str(c) for c in self.children)
+        else:
+            return this
 
     def read(self, fhandle):
         fhandle.seek(self.offset)
         self.contents = fhandle.read(self.size)
-            
-            
-        
-class RHKPageIndexHeader:
-    pass
+
+    def read_children(self, fhandle):
+        for child in self.children:
+            #            print(self.offset, child.objtype, child.size)
+            child.read(fhandle)
 
 
-class RHKPageIndexArray:
-    pass
+class RHKPageIndexHeader:  # Object Id: 1
+    '''.. py::class:: RHKPageIndexHeader
+
+    Class for RHK Page Index Header
+
+    The page index header stores the details of page index array,
+which contains the array of page offsets and other info.  Using the
+index array we can locate the required page data, thumbnail data for
+the respective page, etc without reading th eentire SM4 file
+
+    See rhk_sm4_read_page_index_header in rhk-sm4.c
+
+    Attributes
+    ----------------
+    pagecount, children, reserved
+    '''
+    packer = ExtStruct('<4I')
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        header = RHKPageIndexHeader.packer.unpack_from_file(fhandle)
+        self.pagecount = header[0]
+        self.children = get_objects_from_list(fhandle, header[1], self)
+        self.reserved = header[2:]
+        self.read_children(fhandle)
+
+    def __str__(self):
+        return 'RHKPageIndexHeader:@{0.offset} x {0.size}\n  '.format(self) + \
+            "\n".join(str(child) for child in self.children)
 
 
-class RHKPageHeader:
-    pass
+class RHKPage:
+    '''.. py::class::RHKPage
+
+    Class for RHK Page
+
+    Attributes
+    ----------------
+         page_id, datatype, datatype_name,
+         soucetype, sourcetype_name, pagecount, children, reserved
 
 
-class RHKStringData:
-    pass
+      Object List: Stores the Page Index Objects.  Currently we are storing:
+        1. Page Header
+        2. Page Data
+        3. Thumbnail
+        4. Thumbnail header
+    '''
+
+    packer = ExtStruct('<16s4I')
+
+    def __init__(self, fhandle):
+        datatypes = ['image data', 'line/spectra data', 'xy_data',
+                     'annoted line/spectral data', 'text_data',
+                     'text_annotate', 'Sequential_data']
+        sourcetypes = ['raw page', 'processed page',
+                       'calculated page', 'imported page']
+        self.page_id, self.datatype, self.sourcetype, \
+            self.objcount, self.minorversion \
+            = RHKPage.packer.unpack_from_file(fhandle)
+        self.datatype_name = datatypes[self.datatype]
+        self.sourcetype_name = sourcetypes[self.sourcetype]
+        self.children = get_objects_from_list(fhandle, self.objcount, self)
+
+    def read(self, fhandle):
+        for child in self.children:
+            child.read(fhandle)
+
+    def __str__(self):
+        return "RHKPage:\n"+"\n".join(str(child) for child in self.children)
+
+
+class RHKPageIndexArray:  # Object Id: 2
+    '''.. py::class:: RHKPageIndexArray
+
+    Class for RHK Page Index Array
+    '''
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        self.pages = [RHKPage(fhandle) for i in range(self.parent.pagecount)]
+        for page in self.pages:
+            page.read(fhandle)
+
+    def __str__(self):
+        this = 'RHKPageIndexArray @ {0.offset} x {0.size}'.format(self)
+        that = '\n'.join(str(page) for page in self.pages)
+        return this + "\n" + that
+
+
+class RHKPageHeader:  # Object id: 3
+    '''.. py::class:: RHKPageHeader
+
+    Class for RHK Page Header
+
+    See rhk_sm4_read_page_header in rhk-sm4.c
+    '''
+    packer = ExtStruct('<2H3I7iI2i11f3iI64B')
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        self.header = RHKPageHeader.packer.unpack_from_file(fhandle)
+        self.fieldsize = self.header[0]
+        self.strcount = self.header[1]
+        self.page = self.header[2]
+        self.datasubsource = self.header[3]
+        self.linetype = self.header[4]
+        self.x_coord = self.header[5]
+        self.y_coord = self.header[6]
+        self.x_size = self.header[7]
+        self.y_size = self.header[8]
+        self.image_type = self.header[9]
+        self.scan_dir = self.header[10]
+        self.group_id = self.header[11]
+        self.data_size = self.header[12]
+        self.min_z_value = self.header[0]
+        self.max_z_value = self.header[14]
+        # float
+        # scale
+        self.x_scale = self.header[15]
+        self.y_scale = self.header[16]
+        self.z_scale = self.header[17]
+        self.xy_scale = self.header[18]
+        # offset
+        self.x_offset = self.header[19]
+        self.y_offset = self.header[20]
+        self.z_offset = self.header[21]
+        #
+        self.period = self.header[22]
+        self.bias = self.header[23]
+        self.current = self.header[24]
+        self.angle = self.header[25]
+        # int
+        self.colorinfocount = self.header[26]
+        self.grid_x_size = self.header[27]
+        self.grid_y_size = self.header[28]
+        self.objcount = self.header[29]
+        #
+        self.children = get_objects_from_list(fhandle, self.objcount, self)
+        self.read_children(fhandle)
+
+    def __str__(self):
+        return 'RHKPageHeader @ {0.offset} x {0.size}\n  '.format(self) + \
+            '\n  '.join(str(child) for child in self.children)
+
+
+class RHKPageData:
+    ''' .. py::class:: RHKPageData
+
+     Class for RHK Page data.
+     the most important information (Mapping data, spectral data)
+     '''
+
+    def read(self, fhandle):
+        if self.parent.datatype == 0:
+            packstr = "<" + str(self.size // 4) + "l"
+            RHKPageData.packer = ExtStruct(packstr)
+            fhandle.seek(self.offset)
+            self.data = RHKPageData.packer.unpack_from_file(fhandle)
+        else:
+            pass  # not implemented for spectrum
+
+    def __str__(self):
+        return 'RHKPageData: @{0.offset} x {0.size}\n  '.format(self)
+
+
+class RHKStringData:    # Object id: 10
+    packer = ExtStruct('<H')
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        self.strings = []
+        for ins in range(self.parent.strcount):
+            strlen = RHKStringData.packer.unpack_from_file(fhandle)[0]
+            self.strings.append(fhandle.read(strlen*2).decode("utf-16"))
+
+    def __str__(self):
+        return "RHKStringData @ {0.offset} x {0.size}\n ".format(self) +\
+            "\n ".join(self.strings)
+
+
+class RHKPRMHeader:  # Object id: 15
+
+    packer = ExtStruct('<3I')
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        self.header = RHKThumbnailHeader.packer.unpack_from_file(fhandle)
+        self.compression = self.header[0]
+        self.originalsize = self.header[1]
+        self.compressionsize = self.header[2]
+
+    def __str__(self):
+        return 'RHKPRMHeader @ {0.offset} x {0.size}\n '.format(self)
+
+
+class RHKThumbnailHeader:  # Object id: 16
+
+    packer = ExtStruct('<3I')
+
+    def read(self, fhandle):
+        fhandle.seek(self.offset)
+        self.header = RHKThumbnailHeader.packer.unpack_from_file(fhandle)
+        self.width = self.header[0]
+        self.height = self.header[1]
+        self.nformat = self.header[2]
+
+    def __str__(self):
+        return "RHKThumbnailHeader @ {0.offset} x {0.size}\n ".format(self)
 
 
 RHKObject.registObjType(1, RHKPageIndexHeader)
 RHKObject.registObjType(2, RHKPageIndexArray)
 RHKObject.registObjType(3, RHKPageHeader)
-#RHKObject.registObjType(4, RHKPageData)
-#RHKObject.registObjType(5, RHKImageDriftHeader)
-#RHKObject.registObjType(6, RHKImageDrift)
-#RHKObject.registObjType(7, RHKSpecDriftHeader)
-#RHKObject.registObjType(8, RHKSpecDriftData)
-#RHKObject.registObjType(9, RHKColorInfo)
+RHKObject.registObjType(4, RHKPageData)
+# RHKObject.registObjType(5, RHKImageDriftHeader)
+# RHKObject.registObjType(6, RHKImageDrift)
+# RHKObject.registObjType(7, RHKSpecDriftHeader)
+# RHKObject.registObjType(8, RHKSpecDriftData)
+# RHKObject.registObjType(9, RHKColorInfo)
 RHKObject.registObjType(10, RHKStringData)
-#RHKObject.registObjType(11, RHKTipTrackHeader)
-#RHKObject.registObjType(12, RHKTipTrackData)
-#RHKObject.registObjType(13, RHKPRM)
-#RHKObject.registObjType(14, RHKThumbnail)
-#RHKObject.registObjType(15, RHKPRMHeader)
-#RHKObject.registObjType(16, RHKThumbnailHeader)
+# RHKObject.registObjType(11, RHKTipTrackHeader)
+# RHKObject.registObjType(12, RHKTipTrackData)
+# RHKObject.registObjType(13, RHKPRM)
+# RHKObject.registObjType(14, RHKThumbnail)
+RHKObject.registObjType(15, RHKPRMHeader)
+RHKObject.registObjType(16, RHKThumbnailHeader)
 
 
 class SM4File:
@@ -95,18 +316,24 @@ class SM4File:
     packer = ExtStruct('<36s5I')
 
     def __init__(self, filename):
-        with open(filename, 'rb') as f:
-            f.seek(0)
-            headersize = struct.unpack('H', f.read(2))[0]
-            header = SM4File.packer.unpack_from_file(f)
+        if isinstance(filename, str):
+            fhandle = open(filename, 'rb')
+        elif isinstance(filename, io.IOBase):
+            fhandle = filename
+        with fhandle:
+            fhandle.seek(0)
+            headersize = struct.unpack('H', fhandle.read(2))[0]
+            header = SM4File.packer.unpack_from_file(fhandle)
             if headersize > SM4File.packer.size:
-                self.header_pad = f.read(headersize - SM4File.packer.size)
+                self.header_pad = fhandle.read(headersize -
+                                               SM4File.packer.size)
             self.signature = header[0]
             self.pagecount = header[1]
-            self.children = get_objects_from_list(f, header[2], self)
+            self.children = get_objects_from_list(fhandle, header[2], self)
             self.reserved = header[4:]
             for child in self.children:
-                child.read(f)
+                child.read(fhandle)
+
 
 if __name__ == "__main__":
     import sys
--- rhk/test/rhk/rhksm4_test.py
+++ rhk/test/rhk/rhksm4_test.py
@@ -3,7 +3,9 @@
 import os
 from nose.tools import with_setup
 from nose.tools import eq_
+from nose.tools import ok_
 import rhk.rhksm4
+
 
 class TestSM4(object):
     '''Class for test of rhksm4 module
@@ -12,13 +14,143 @@ class TestSM4(object):
     '''
     def setup(self):
         datadir = os.path.abspath(os.path.dirname(__file__)) + '/data/'
+        #
         data_file1 = datadir + 'data3293FFT.sm4'
         self.fft = rhk.rhksm4.SM4File(data_file1)
-
+        #
+        data_file2 = datadir + 'Co_Ru0001_1300.SM4'
+        data2 = open(data_file2, 'rb')
+        self.co_ru = rhk.rhksm4.SM4File(data2)
 
     @with_setup(setup=setup)
     def test_fileheader(self):
+        #
         eq_('STiMage 005.004 1\x00', self.fft.signature.decode('utf-16'))
         eq_(2, self.fft.pagecount)
         eq_(3, len(self.fft.children))
-        
+        #
+        eq_('STiMage 005.004 1\x00', self.co_ru.signature.decode('utf-16'))
+        eq_(6, self.co_ru.pagecount)
+        eq_(3, len(self.co_ru.children))
+
+    @with_setup(setup=setup)
+    def test_PageIndexHeader(self):
+        #
+        pih = self.fft.children[0]
+        eq_(1, pih.objtype)
+        eq_('PageIndexHeader', pih.objname)
+        eq_(2, pih.pagecount)
+        eq_(1, len(pih.children))
+
+    @with_setup(setup=setup)
+    def test_PageIndexArray(self):
+        #
+        pia = self.fft.children[0].children[0]
+        eq_(2, pia.objtype)
+        eq_('PageIndexArray', pia.objname)
+        eq_(2, len(pia.pages))
+        eq_(0, len(pia.children))
+        #
+        pia2 = self.co_ru.children[0].children[0]
+        eq_(2, pia2.objtype)
+
+    @with_setup(setup=setup)
+    def test_Page(self):
+        #
+        page = self.fft.children[0].children[0].pages[0]
+        ok_(isinstance(page, rhk.rhksm4.RHKPage))
+        eq_('image data', page.datatype_name)
+        eq_('processed page', page.sourcetype_name)
+        eq_(4, page.objcount)
+
+    @with_setup(setup=setup)
+    def test_PageHeader(self):
+        #
+        ph = self.fft.children[0].children[0].pages[0].children[0]
+        ok_(isinstance(ph, rhk.rhksm4.RHKObject))
+        #
+        eq_('PageHeader', ph.objname)
+        #
+        eq_(180, ph.fieldsize)
+        eq_(17, ph.strcount)
+        eq_(6, ph.page)  # 6 means image FFT transform
+        eq_(0, ph.datasubsource)
+        eq_(0, ph.linetype)
+        eq_(0, ph.x_coord)
+        eq_(0, ph.y_coord)
+        eq_(256, ph.x_size)
+        eq_(256, ph.y_size)
+        eq_(0, ph.image_type)
+        eq_(1, ph.scan_dir)
+        eq_(54301564, ph.group_id)
+        eq_(262144, ph.data_size)  # 256 x 256 x 4 (4 is float bytes)
+        eq_(180, ph.min_z_value)
+        eq_(1407, ph.max_z_value)
+        #  physical values
+        eq_(-20000002.0, ph.x_scale)  # X physical units per pixel or bit
+        eq_(-20000002.0, ph.y_scale)  # Y physical units per pixel or bit
+        eq_(1.7287545688304817e-06, ph.z_scale)
+        eq_(0, ph.xy_scale)
+        #  offset
+        eq_(0, ph.x_offset)
+        eq_(0, ph.y_offset)
+        eq_(0.0033952740486711264, ph.z_offset)
+        #  Measurement conditions
+        eq_(0.011718700639903545, ph.period)  # Period 0.011719 s
+        eq_(0.16017913818359375, ph.bias)  # Bias 0.160179 V
+        eq_(1.995468235094222e-09, ph.current)  # 2 nA
+        eq_(0, ph.angle)
+        #
+        eq_(1, ph.colorinfocount)
+        eq_(0, ph.grid_x_size)
+        eq_(0, ph.grid_y_size)
+        eq_(9, ph.objcount)
+
+        eq_('StringData', ph.children[0].objname)
+        eq_(5, ph.children[1].objtype)
+        eq_(6, ph.children[2].objtype)
+        eq_(0, ph.children[3].objtype)
+        eq_(0, ph.children[4].objtype)
+        eq_(9, ph.children[5].objtype)
+        eq_(11, ph.children[6].objtype)
+        eq_(0, ph.children[7].objtype)
+        eq_(17, ph.children[8].objtype)
+
+    @with_setup(setup=setup)
+    def test_PageData(self):
+        pd = self.fft.children[0].children[0].pages[0].children[1]
+        ok_(isinstance(pd.data, tuple))
+        eq_(256*256, len(pd.data))
+
+    def test_StringData(self):
+        stringdata =\
+            self.fft.children[0].children[0].pages[0].children[0].children[0]
+        eq_('FFT image', stringdata.strings[0])
+        eq_('Page 14 of 26, 0.00 V', stringdata.strings[1])
+        eq_('WTe2 4.78K  sens=50 mod=20mV 616Hz TC=3', stringdata.strings[2])
+        eq_('', stringdata.strings[3])
+        eq_('D:\\SPMdata\\20160421\\data3293FFT.sm4', stringdata.strings[4])
+        eq_('04/22/16', stringdata.strings[5])
+        eq_('11:01:44', stringdata.strings[6])
+        eq_('m-1', stringdata.strings[7])
+        eq_('m-1', stringdata.strings[8])
+        eq_('V', stringdata.strings[9])
+        for i in range(10, 13):
+            eq_('', stringdata.strings[i])
+        eq_('0256', stringdata.strings[13])
+        eq_('337', stringdata.strings[14])
+        eq_('', stringdata.strings[15])
+        eq_('', stringdata.strings[16])
+
+    def test_ThumbnailHeader(self):
+        tnh = self.fft.children[0].children[0].pages[0].children[3]
+        ok_(128, tnh.width)
+        ok_(128, tnh.height)
+#
+
+    def test_PRMHeader(self):
+        prmh = self.fft.children[2]
+        eq_(3, len(prmh.header))
+        eq_(1, prmh.header[0])
+        eq_(67092, prmh.header[1])
+        eq_(13521, prmh.header[2])
