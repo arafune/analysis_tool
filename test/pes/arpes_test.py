56acb155596a5321912c20c1963c994054f3cad0
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2017-03-19 17:23:09 +0900

    Update test_angle_to_k Test for ARPESmap -> ARPESband method.    * But unfortunately, the method itself is not implemented.

--- test/pes/arpes_test.py
+++ test/pes/arpes_test.py
@@ -37,6 +37,7 @@ class TestARPES(object):
             np.array([5.1 + i * 0.01 for i in range(211)]),
             self.splab041.groups[0].regions[0].energy_axis)
 
+    @with_setup(setup=setup)
     def test_angle_and_energy_shift(self):
         self.arpes041.angle_shift(4.0)
         np.testing.assert_allclose(
@@ -47,9 +48,16 @@ class TestARPES(object):
             np.array([2.1, 2.11, 2.12, 2.13]),
             self.arpes041.energy_axis[0:4], rtol=1e-06)
 
-
+    @with_setup(setup=setup)
     def test_angle_to_k(self):
-        '''Test for ARPESmap method 
+        '''Test for ARPESmap -> ARPESband method 
         '''
-        pass
+        energymax = np.max(self.arpes041.energy_axis)
+        energymin = np.min(self.arpes041.energy_axis)
+        kmax = 0.512 * np.sqrt(energymax) * np.sin(
+            np.max(self.arpes041.angle_axis) * np.pi/180)
+        kmin = 0.512 * np.sqrt(energymin) * np.sin(
+            np.min(self.arpes041.angle_axis) * np.pi/180)        
+        eq_(kmax, 0.51293896353914326)
+        eq_(kmin, 0)
     #k = self.arpes041.angle_to_k()
