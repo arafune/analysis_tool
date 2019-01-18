17820d4bbc652276b6e987c7f9763e61c332254d
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-01-18 15:07:00 +0900

    LTlog.py: first commit

---
+++ raspberry/LTmeasure/LTlog.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding: 
+"""LakeShore 330 から温度データを読み込んで、png ファイルフォーマットのグラフを出力する。"""
+
+import datetime
+import argparse
+import matplotlib
+
+ 
+def draw_lakeshore330(data):
+    """1st column が datetime オブジェクトの2Dデータを読み込んでグラフにする。"""
+    pass
+
+if __name__ == '__main__':
+    pass
