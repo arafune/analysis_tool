d1a6f92e8057a8fd069bfb616b46e056376c45f2
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-01-19 07:33:43 +0900

    add read_dummy function

--- raspberry/LTmeasure/LTlog.py
+++ raspberry/LTmeasure/LTlog.py
@@ -4,7 +4,8 @@
 
 import datetime
 import argparse
-import matplotlib
+import matplotlib.pyplot as plt
+import random
 
 
 def build_dummydate():
@@ -12,8 +13,10 @@ def build_dummydate():
     with open('dummydata.dat') as f:
         for line in f:
             l = line.split('\t')
-            datetime.datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S')
-            singledata = [datetime.datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S'),
+            datetime.datetime.strptime(l[0],
+                                       '%Y-%m-%d %H:%M:%S')
+            singledata = [datetime.datetime.strptime(l[0],
+                                                     '%Y-%m-%d %H:%M:%S'),
                           float(l[1]),
                           float(l[2])]
             data.append(singledata)
@@ -21,9 +24,19 @@ def build_dummydate():
     return data
 
 
+def read_dummy(n_ch):
+    '''Return random data
+
+    n_ch: number of channels
+    '''
+    data = [datetime.datetime.now()]
+    data[1:1] = [random.random() for a in range(n_ch)]
+    return data
+
+
 def draw_lakeshore330(data):
     """1st column が datetime オブジェクトの2Dデータを読み込んでグラフにする。"""
-    fig = plt.figure(figsize=(15,10), dpi=200)
+    fig = plt.figure(figsize=(15, 10), dpi=200)
     ax = fig.add_subplot(111)
     ax.plot_date(data[0], data[1], fmt='-', label='Upper sensor')
     ax.plot_date(data[0], data[2], fmt='-', label='Lower sensor')
