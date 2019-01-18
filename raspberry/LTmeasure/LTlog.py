35a9c7d925d54bd28777a0bc8066c91129c75bf6
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-01-18 20:05:26 +0900

    LTlog.py: visualized by using plt.plot_date

--- raspberry/LTmeasure/LTlog.py
+++ raspberry/LTmeasure/LTlog.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
-# coding: 
+# coding:
 """LakeShore 330 から温度データを読み込んで、png ファイルフォーマットのグラフを出力する。"""
 
 import datetime
 import argparse
 import matplotlib
 
- 
+
+def build_dummydate():
+    data = []
+    with open('dummydata.dat') as f:
+        for line in f:
+            l = line.split('\t')
+            datetime.datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S')
+            singledata = [datetime.datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S'),
+                          float(l[1]),
+                          float(l[2])]
+            data.append(singledata)
+            data = list(zip(*data))
+    return data
+
+
 def draw_lakeshore330(data):
     """1st column が datetime オブジェクトの2Dデータを読み込んでグラフにする。"""
-    pass
+    fig = plt.figure(figsize=(15,10), dpi=200)
+    ax = fig.add_subplot(111)
+    ax.plot_date(data[0], data[1], fmt='-', label='Upper sensor')
+    ax.plot_date(data[0], data[2], fmt='-', label='Lower sensor')
+    ax.legend(loc=2)
+    plt.savefig('LTdata.png')
+
 
 if __name__ == '__main__':
     pass
--- raspberry/LTmeasure/dummydata_maker.py
+++ raspberry/LTmeasure/dummydata_maker.py
@@ -4,8 +4,9 @@ import random
 from time import sleep
 
 for i in range(100):
-	now = datetime.datetime.now()
-	a1=random.random()
-	a2=random.random()
-	print("{}\t{}\t{}".format(str(now), a1, a2))
-	sleep(1)
+    now = datetime.datetime.now()
+    now = now.strftime('%Y-%m-%d %H:%M:%S')
+    a1=random.random()
+    a2=random.random()
+    print("{}\t{}\t{}".format(now, a1, a2))
+    sleep(1)
