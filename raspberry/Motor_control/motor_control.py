9cb0e0625d894a1b33a03ae9020fa97b36cfe750
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-01-29 21:54:01 +0900

    Add new files for control and phoibos monitor

---
+++ raspberry/Motor_control/motor_control.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+'''Stepping motor control for Analysis-chamber manipulator and delay line.
+
+Z and theta aixes of Analyais-chamber manipulator can be controlled
+through RS-232C Delay line can be controlled through GPIB
+'''
