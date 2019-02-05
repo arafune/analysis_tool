7523d6bed7a3a76773493978b3777f8e93cd9aff
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-02-05 20:41:13 +0900

    timeout control

--- raspberry/qmass/qmass.py
+++ raspberry/qmass/qmass.py
@@ -32,55 +32,45 @@ class Qmass():
         self.ser.write(b'{000D,10:1FB6')
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
-            time.sleep(1)
+            time.sleep(0.1)
             data_to_read = self.ser.in_waiting
         logger.info(self.ser.read(data_to_read))
         # b'~LM76-00499001,001a,2:0ed1'
         self.ser.write(b'}LM76-00499001,001A,5:1660')
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
-            time.sleep(1)
+            time.sleep(0.1)
             data_to_read = self.ser.in_waiting
         logger.info(self.ser.read(data_to_read))
         # b'~LM76-00499001,0025,3,1,V1.51a,0:262a'
         self.ser.write(b'{0011,55,1,0:402A')
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
-            time.sleep(1)
+            time.sleep(0.1)
             data_to_read = self.ser.in_waiting
         logger.info(self.ser.read(data_to_read))
         # b'~LM76-00499001,001a,2:0ed1'
         self.ser.write(b'}LM76-00499001,001B,15:A7C2')
-        time.sleep(3)
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
-            time.sleep(1)
+            time.sleep(0.1)
             data_to_read = self.ser.in_waiting
         logger.info(self.ser.read(data_to_read))
         # b'~LM76-00499001,001c,6,0:daad'
         self.ser.write(bytes.fromhex('af'))
         self.ser.write(bytes.fromhex('aa'))
-        time.sleep(1)
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
             data_to_read = self.ser.in_waiting
         logger.debug(self.ser.read(data_to_read))  # aa d2
         self.ser.write(bytes.fromhex('ba 03'))
         self.ser.write(bytes.fromhex('a6'))
-        time.sleep(4)
-        data_to_read = self.ser.in_waiting
-        while data_to_read == 0:
-            data_to_read = self.ser.in_waiting
-        logger.debug(self.ser.read(data_to_read))
+        self.ser.timeout = 0.2
+        logger.debug(self.ser.readline())
         # 03 56 61 00 25 03 09 44 03 13 3e 02 2f 6a 03 00 00 01 00 4b 00
         self.ser.write(bytes.fromhex('bb 00 80 80 80 be 0a'))
         self.ser.write(bytes.fromhex('00 ff 00 bf 04'))
-        time.sleep(3)
-        data_to_read = self.ser.in_waiting
-        while data_to_read == 0:
-            time.sleep(1)
-            data_to_read = self.ser.in_waiting
-        logger.debug(self.ser.read(data_to_read))
+        logger.debug(self.ser.readline())
         # 8f 04 19 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 8e
         self.ser.write(bytes.fromhex('a7'))
         data_to_read = self.ser.in_waiting
@@ -89,18 +79,13 @@ class Qmass():
             data_to_read = self.ser.in_waiting
         logger.info(self.ser.read(data_to_read))  # ff 00
         self.ser.write(bytes.fromhex('aa 01 03 10 86 00 a1 00 00 bc'))
-        time.sleep(2)
         data_to_read = self.ser.in_waiting
         while data_to_read == 0:
             data_to_read = self.ser.in_waiting
         logger.debug(self.ser.read(data_to_read))
         # c2 52 85 7f
         self.ser.write(bytes.fromhex('ad 02'))
-        time.sleep(1)
-        data_to_read = self.ser.in_waiting
-        while data_to_read == 0:
-            data_to_read = self.ser.in_waiting
-        logger.debug(self.ser.read(data_to_read))
+        logger.debug(self.ser.readline())
         # 07
         self.ser.write(bytes.fromhex('ad 03'))
         time.sleep(1)
@@ -117,14 +102,12 @@ class Qmass():
         logger.debug(self.ser.read(data_to_read))
         # b2 33 8c bf
         self.ser.write(bytes.fromhex('bf 05'))
-        time.sleep(4)
         data_to_read = self.ser.in_waiting
-        while data_to_read == 0:
-            time.sleep(1)
-            data_to_read = self.ser.in_waiting
-        logger.debug(self.ser.read(data_to_read))
+        logger.debug('data_to_read : {}'.format(data_to_read))
+        logger.debug(self.ser.readline())
         # 8f 05 21 0d 4c 4d 37 36 2d 30 30 34 39 39 30 30 31 00 ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff 8e
         self.ser.write(bytes.fromhex('e6 80 00'))
+        self.ser.timeout = None
         logger.debug('Microvision initialized')
 
     def exit(self):
