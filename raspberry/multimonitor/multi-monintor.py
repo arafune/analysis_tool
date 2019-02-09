1428b360e68ab7124dfc2fc4d5ebcdd6124720be
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-02-09 14:50:38 +0900

    proto-type

--- raspberry/multimonitor/multi-monintor.py
+++ raspberry/multimonitor/multi-monintor.py
@@ -1,31 +1,101 @@
 #!/usr/bin/env python3
 '''Multi channel (7 ch for Voltage, 4 ch for temperature)'''
 
-import time
+from time import sleep
 import datetime
+from logging import getLogger, StreamHandler, DEBUG, Formatter
 import HPADDA
 from Adafruit_MAX31856 import max31856
 
+# logger
+logger = getLogger(__name__)
+fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+formatter = Formatter(fmt)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.propagate = False
 
 def pressure(volt):
     '''Return the pressure (mbar) from the monitor voltage'''
-    pass
+    exponent = int[volt] - 11
+    mantissa = ((volt - int[volt]) + .1)/.11
+    return mantissa * 10**exponent
 
 adda = HPADDA.Board()
 adda.set_sample_rate(300)
-THERMOCOUPLES = [max31856.MAX31856(software_spi={'clk':25, 'cs':14, 'do':8, 'di':7}),
-                 max31856.MAX31856(software_spi={'clk':25, 'cs':15, 'do':8, 'di':7}),
-                 max31856.MAX31856(software_spi={'clk':25, 'cs':16, 'do':8, 'di':7}),
-                 max31856.MAX31856(software_spi={'clk':25, 'cs':21, 'do':8, 'di':7})]
+THERMOCOUPLES = [max31856.MAX31856(software_spi={'clk':25, 'cs':14,
+                                                 'do':8, 'di':7}),
+                 max31856.MAX31856(software_spi={'clk':25, 'cs':15,
+                                                 'do':8, 'di':7}),
+                 max31856.MAX31856(software_spi={'clk':25, 'cs':16,
+                                                 'do':8, 'di':7}),
+                 max31856.MAX31856(software_spi={'clk':25, 'cs':21,
+                                                 'do':8, 'di':7})]
 
 
+def read_temperatures():
+    '''Return temperatue data.
 
-def single_read():
-    #voltages = [ adda.get_voltage(ch) for ch in range(8) ]
-    voltages =adda.get_voltage(6)
-    temperatures = [THERMOCOUPLES[i].read_temp_c() for i in range(4)]
-    print ('{:.4e}'.format(voltages))
-    print(temperatures)
+    '''
+    external = [THERMOCOUPLES[i].read_temp_c() for i in range(4)]
+    internal = [THERMOCOUPLES[i].internal_temp_c() for i in range(4)]
+    ret = []
+    for i in range(4):
+        ret.append(external[i], internal[i])
+    return ret
 
-while True:
-    single_read()
+def read_ion_gauge(chamber=0):
+    '''Read ion gauge data
+
+    Parameters
+    -----------
+    chamber: int
+        0: Analysis
+        1: Preparation
+
+    Return
+    -------
+    float
+        Pressure (mbar)
+    '''
+    voltage = adda.get_voltages(chamber+1)
+    return pressure(voltage)
+
+LOGFILE = open('log.txt', mode='w')
+SAVE_FMT = '{} {:6.3f}\t{:6.3f}\t{:6.3f}\t{:6.3f}'
+SAVE_FMT += '\t{:.3e}\t{:.3e}\t{:6.3f}\t{:6.3f}\t{:6.3f}'
+
+def scan_and_save():
+    temperatures = read_temperatures()
+    ana_pres = read_ion_gauge(0)
+    prep_pres = read_ion_gauge(1)
+    v3 = adda.get_voltage(5)
+    v4 = adda.get_voltage(6)
+    v5 = adda.get_voltage(7)
+    temp_fmt = 'Temperature at {}: {:6.3f} C (internal {:6.3f} C)'
+    pressure_fmt = '{:.3e} mbar'
+    votage_fmt = '{:5.2f} V'
+    for i in range(4):
+        logger.debug(temp_fmt.format(i, temperatures[i][0],
+                                        temperatures[i][1]))
+    logger.debug('Analysis: ' + pressure_fmt.format(ana_pres))
+    logger.debug('Preparation: ' + pressure_fmt.format(prep_pres))
+    logger.debug('Voltage-3:' + voltage_fmt.format(v3))
+    logger.debug('Voltage-4:' + voltage_fmt.format(v4))
+    logger.debug('Voltage-5:' + voltage_fmt.format(v5))
+    now = datetime.datetime.strftime(
+            datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+    LOG.write(SAVE_FMT.format(now,
+                              temperatures[0][0], temperatures[1][0],
+                              temperatures[2][0], temperatures[3][0],
+                              ana_pres, prep_pres, v3, v4, v5))
+
+try:
+    while True:
+        scan_and_save()
+        sleep(1)
+except KeyboardInterrupt:
+    LOG.close()
