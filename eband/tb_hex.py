cb01a1d04d11e28cec6273b7324cf013eaa12e00
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2019-09-05 08:19:18 +0900

    New function: distance

--- eband/tb_hex.py
+++ eband/tb_hex.py
@@ -7,7 +7,7 @@ import numpy as np
 a_size = 1000
 #
 k_vec_gm = np.zeros((2, a_size))
-k_vec_mk = np.zeros((2, int(a_size / np.ceil(a_size))))
+k_vec_mk = np.zeros((2, int(np.ceil(a_size / np.sqrt(3)))))
 k_vec_kg = np.zeros((2, a_size))
 #
 lattice = 1.5
@@ -18,13 +18,24 @@ t = 1.0
 k_vec_gm[0] = np.linspace(0, 2 * np.pi / (lattice * np.sqrt(3)), a_size)
 k_vec_mk[0] = k_vec_gm[0][-1]
 k_vec_mk[1] = np.linspace(0, 2 * np.pi / (lattice * 3.0),
-                          np.ceil(a_size / np.sqrt(3)))
+                          int(np.ceil(a_size / np.sqrt(3))))
 k_vec_kg[0] = np.linspace(2 * np.pi / (lattice * np.sqrt(3)), 0, a_size)
 k_vec_kg[1] = np.linspace(2 * np.pi / (lattice * 3), 0, a_size)
 
 
+def distance(kdata):
+    """Return the distace."""
+    dist = np.linspace(
+        0,
+        np.sqrt((kdata[0][0] - kdata[0][-1])**2 +
+                (kdata[1][0] - kdata[1][-1])**2), kdata.shape[1])
+    return dist
+
+
 def energy(kx, ky, eps_1=eps_en_1, eps_2=eps_en_2, t=t, lattice=lattice):
-    """Return the energy from the tight binding method.
+    r"""Return the energy from the tight binding method.
+
+    :math:`\frac{\epsilon_1 + \epsilon_2}{2} \pm \sqrt{ \frac{(\epsilon_1 - \epsilon_2)^2}{4} + 4t^2 \left( \frac{1}{4} + \cos\left(\frac{\sqrt{3}k_x}{2} a \right)  \cos\left(\frac{k_y}{2} a  \right) + \cos ^2\left( \frac{k_y}{2} a \right) \right)}`
 
     Parameters
     -----------
