24e8faecdf320df4053dbf0d6021b83c3db1affc
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2020-05-13 14:51:58 +0900

    calib1d: new methods for writing

--- pes/calib1d.py
+++ pes/calib1d.py
@@ -2,6 +2,8 @@
 """Module to analyze and show SPECS calib1d data."""
 
 import numpy as np
+from collections import OrderedDict
+from datetime import datetime
 from scipy import interpolate
 
 
@@ -13,6 +15,9 @@ class Calib1d:
     
     """
 
+    SL_Version = "4.57.1-r83491"
+    SL_Build_Date = "2019-06-19 10:39:02 UTC"
+
     def __init__(self, file_name=None):
         """Initialization.
         
@@ -23,7 +28,7 @@ class Calib1d:
         """
         self.positions = []
         self.shifts = []
-        self.header = []
+        self.header = OrderedDict()
         if file_name is None:
             self.creation_data = ""
             self.sl_version = ""
@@ -38,11 +43,33 @@ class Calib1d:
             with open(file_name, "r") as fileread:
                 for line in fileread:
                     if line[0] == "#":
-                        self.header.append(line)
+                        self.read_header(line)
                     else:
-                        line.strip()
                         data = line.split(" ")
                         self.positions.append(float(data[0]))
                         self.shifts.append(float(data[1]))
                 self.positions = np.array(self.positions)
                 self.shifts = np.array(self.shifts)
+
+    def read_header(self, line):
+        """Read header."""
+        if "=" in line:
+            item, value = line.split("=")
+            self.header[item] = value
+        else:
+            self.header[line] = None
+
+    def write_header(self):
+        """Build header."""
+        self.header["# Creation Date "] = ' "{}"'.format(
+            datetime.strftime(datetime.utcnow(), "%Y-%m-%d %H:%M:%S UTC")
+        )
+        for k, v in self.header.items():
+            if v is None:
+                print(k)
+            else:
+                v.strip()
+                print(k + "=" + v)
+
+    def save(self, filename):
+        pass
