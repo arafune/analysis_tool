63888e39b27a5a0ffee613b79dfa885f9246ec18
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2020-07-11 10:11:13 +0900

    Work

--- script/IRSpectrum.sh
+++ script/IRSpectrum.sh
@@ -10,12 +10,12 @@ PHONOPYIR="${HOME}/src/Phonopy-Spectroscopy/Scripts/phonopy-ir"
 
 phonopy --fc $1 --hdf5
 phonopy --dim="1 1 1"  --fc-symmetry --mesh="1 1 1" --eigenvectors --readfc --hdf5
-phonopy-vasp-born $2 > BORN
+phonopy-vasp-born --dim="1 1 1" --nost $2 > BORN
 phonopy --dim="1 1 1" --readfc --hdf5 --fc-symmetry --irreps="0 0 0"
-$PHONOPYIR --ir_reps --ir_reps_yaml  irreps.yaml --linewidth=32 --data_format csv
+$PHONOPYIR --ir_reps --ir_reps_yaml  irreps.yaml --linewidth=8 --data_format csv
 mv IR-Spectrum.png IR-Spectrum.cm-1.png
 mv IR-Spectrum.dat IR-Spectrum.cm-1.dat
-mv IR-Spectrum.csv IR-Spectrum.cm-1.cvs
+mv IR-Spectrum.csv IR-Spectrum.cm-1.csv
 mv IR-PeakTable.csv IR-PeakTable.cm-1.csv
 mv IR-PeakTable.dat IR-PeakTable.cm-1.dat
 $PHONOPYIR --freq_units meV --ir_reps --ir_reps_yaml irreps.yaml --linewidth=32 --data_format csv
