49e6dc2a31dadf6f7de28431af08d82f678f136e
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2021-03-22 18:48:07 +0900

    New module: eels.basic

---
+++ eels/basic.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+"""HREELS basic parameters"""
+
+import numpy as np
+
+
+DEGREE: float = np.pi / 180.0
+
+
+def scatter_angle(theta_in_deg: float) -> float:
+    """Return the minimum scattering angle.
+
+    The angle of of rotation about the detector is 78 degree measured from straight geometry.
+
+    Parameters
+    --------------
+    theta_in_deg: float
+        incident angle
+
+    Returns
+    --------
+    float
+        Minimum scattering angle achieved by IB500
+    """
+    return 102 - theta_in_deg
+
+
+def momentum_transfer(
+    energy_in_eV: float, theta_in_deg: float, theta_out_deg: float
+) -> float:
+    """Return the momentum transfer in EELS experiment.
+
+    Parameters
+    ---------------
+    energy_in: float
+        Energy of impact electron (eV unit)
+    theta_in_deg: float
+        Incident angle
+    theta_out_deg: float
+        scattering angle
+
+    Returns
+    -------
+    float
+        Transfered momentum (AA-1 units)
+    """
+    return (
+        0.512
+        * np.sqrt(energy_in_eV)
+        * (np.sin(theta_in_deg * DEGREE) - np.sin(theta_out_deg * DEGREE))
+    )
