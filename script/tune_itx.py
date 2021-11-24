ae4b86c05ec5ea82fd59b48fe4d2bbd7edc73ab8
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2021-11-24 12:30:46 +0900

    Excitation energy や user comment を Wave の NOTE に加える。

--- script/tune_itx.py
+++ script/tune_itx.py
@@ -22,16 +22,40 @@ if not specified, use standard output""",
     )
     parser.add_argument("itx_file")
     args = parser.parse_args()
+    user_comment: str = ""
     id: int
     with open(args.itx_file) as itx_file:
         if args.output:
             output = open(args.output, "w")
         for line in itx_file:
-            if "//Spectrum ID" in line:
+            if line.startswith("X //Spectrum ID"):
                 id = int(line.split("=")[1])
+            if "User Comment" in line:
+                try:
+                    user_comment = line.split("=", maxsplit=1)[1].strip()
+                except IndexError:
+                    user_comment = ""
+            if line.startswith("X //Kinetic Energy"):
+                excitation_energy: str = line.split("=", maxsplit=1)[1].strip()
             if line.startswith("WAVES/S/N"):
                 command_part: str = line.split(maxsplit=1)[0]
                 line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
+            if line.startswith("END") and user_comment:
+                line = (
+                    "END\r\n"
+                    + "X Note /NOCR "
+                    + "'ID_"
+                    + str(id).zfill(3)
+                    + "'"
+                    + ' "'
+                    + user_comment
+                    + '"'
+                    + "\r\n"
+                )
+                line += (
+                    "X Note /NOCR " + "'ID_" + str(id).zfill(3) + "'" + ' "'
+                    r"\r\nExcitation_energy:" + excitation_energy + '"' + "\r\n"
+                )
             if line.startswith("X SetScale"):
                 command_part = " ".join(line.split()[:-1])
                 line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
