4f6656e8d331d78e8419db412a494e348348443a
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2021-12-22 15:19:00 +0900

    Add new file

---
+++ pes/lpes_basic.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Module to analyze and show ARPES data."""
+
+from typing import Union
+
+
+def wavelength2eV(wavelength_nm: float) -> float:
+    """Return Energy of the light
+
+    Parameters
+    ----------
+    wavelength_nm : float
+        [description]
+
+    Returns
+    -------
+    float
+        [description]
+    """
+    planck_const: float = 6.62607015e-34  ##
+    planck_const_eV: float = 4.135667696e-15
+    light_velocity: int = 299792458
+
+    #  (h*c = 1.2398419840550368e-6)
+    return planck_const_eV * light_velocity / (wavelength_nm * 1e-9)
