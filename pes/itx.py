90792ec66e077b9451cf7a563a399f2d2b3ca95b
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-06-30 11:28:54 +0900

    Fix wrong indent and others.  (itx.py)

--- pes/itx.py
+++ pes/itx.py
@@ -19,7 +19,7 @@ import sys
 import argparse
 
 
-def tune(itx_file) -> str:
+def tune(itx_file, angle_correction: float | None = None) -> str:
     """_summary_
 
     Parameters
@@ -35,58 +35,60 @@ def tune(itx_file) -> str:
     modified_itx_file = []
     for line in itx_file:
         if line.startswith("X //Spectrum ID"):
-                id = int(line.split("=")[1])
-            if "User Comment" in line:
-                try:
-                    user_comment = line.split("=", maxsplit=1)[1].strip()
-                except IndexError:
-                    user_comment = ""
-            if line.startswith("X ///Excitation Energy"):
-                excitation_energy = line.split("=", maxsplit=1)[1].strip()
-            if line.startswith("WAVES/S/N"):
-                command_part: str = line.split(maxsplit=1)[0]
-                line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
-            if line.startswith("END") and user_comment:
-                line = (
-                    "END\r\n"
-                    + "X Note /NOCR "
-                    + "'ID_"
-                    + str(id).zfill(3)
-                    + "'"
-                    + ' "'
-                    + user_comment
-                    + '"'
-                    + "\r\n"
-                )
-                line += (
-                    "X Note /NOCR " + "'ID_" + str(id).zfill(3) + "'" + ' "'
-                    r"\r\nExcitation_energy:" + excitation_energy + '"' + "\r\n"
-                )
-            if line.startswith("X SetScale/I x"):
-                if args.angle_correction:
-                    ## 1.3088 が 2021/11/24の解析から求めた値
-                    setscalex: list[str] = line.split()
-                    new_scale_x_left = float(setscalex[3][:-1]) / args.angle_correction
-                    new_scale_x_right = float(setscalex[4][:-1]) / args.angle_correction
-                    note: str = r"""X Note /NOCR 'ID_{:03}' "\r\nangle_correction:{}" """.format(
+            id = int(line.split("=")[1])
+        if "User Comment" in line:
+            try:
+                user_comment = line.split("=", maxsplit=1)[1].strip()
+            except IndexError:
+                user_comment = ""
+        if line.startswith("X ///Excitation Energy"):
+            excitation_energy = line.split("=", maxsplit=1)[1].strip()
+        if line.startswith("WAVES/S/N"):
+            command_part: str = line.split(maxsplit=1)[0]
+            line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
+        if line.startswith("END") and user_comment:
+            line = (
+                "END\r\n"
+                + "X Note /NOCR "
+                + "'ID_"
+                + str(id).zfill(3)
+                + "'"
+                + ' "'
+                + user_comment
+                + '"'
+                + "\r\n"
+            )
+            line += (
+                "X Note /NOCR " + "'ID_" + str(id).zfill(3) + "'" + ' "'
+                r"\r\nExcitation_energy:" + excitation_energy + '"' + "\r\n"
+            )
+        if line.startswith("X SetScale/I x"):
+            if angle_correction:
+                ## 1.3088 が 2021/11/24の解析から求めた値
+                setscalex: list[str] = line.split()
+                new_scale_x_left = float(setscalex[3][:-1]) / args.angle_correction
+                new_scale_x_right = float(setscalex[4][:-1]) / args.angle_correction
+                note: str = (
+                    r"""X Note /NOCR 'ID_{:03}' "\r\nangle_correction:{}" """.format(
                         id, args.angle_correction
                     )
-                    command_part = " ".join(line.split()[:-1])
-                    line = (
-                        note
-                        + """\r\nX SetScale/I x, {}, {}, {} {} 'ID_{:03}'\r\n""".format(
-                            new_scale_x_left,
-                            new_scale_x_right,
-                            setscalex[5],
-                            setscalex[6],
-                            id,
-                        )
+                )
+                command_part = " ".join(line.split()[:-1])
+                line = (
+                    note
+                    + """\r\nX SetScale/I x, {}, {}, {} {} 'ID_{:03}'\r\n""".format(
+                        new_scale_x_left,
+                        new_scale_x_right,
+                        setscalex[5],
+                        setscalex[6],
+                        id,
                     )
-                else:
-                    command_part = " ".join(line.split()[:-1])
-                    line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
-            if line.startswith("X SetScale/I y") or line.startswith("X SetScale/I d"):
+                )
+            else:
                 command_part = " ".join(line.split()[:-1])
                 line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
-            modified_itx_file.append(line.strip()+"\r\n")
+        if line.startswith("X SetScale/I y") or line.startswith("X SetScale/I d"):
+            command_part = " ".join(line.split()[:-1])
+            line = command_part + " 'ID_" + str(id).zfill(3) + "'\r\n"
+        modified_itx_file.append(line.strip() + "\r\n")
     return "".join(modified_itx_file)
