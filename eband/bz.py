9f2796876654c632791b18463a779da81dc49fc7
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-07-04 15:06:47 +0900

    new file:   bz.py

---
+++ eband/bz.py
@@ -0,0 +1,46 @@
+from scipy.spatial import Voronoi
+import numpy as np
+
+
+def get_bz_3d(cell):
+    """
+    Generate the Brillouin Zone of a given cell.
+    The BZ is the Wigner-Seitz cell of the reciprocal lattice,
+    which can be constructed by Voronoi decomposition
+    to the reciprocal lattice.
+    A Voronoi diagram is a subdivision of the space
+    into the nearest neighborhoods of a given set of points.
+
+    https://en.wikipedia.org/wiki/Wigner%E2%80%93Seitz_cell
+    https://docs.scipy.org/doc/scipy/reference/tutorial/spatial.html#voronoi-diagrams
+    """
+
+    cell = np.asarray(cell, dtype=float)
+    assert cell.shape == (3, 3)
+
+    px, py, pz = np.tensordot(cell, np.mgrid[-1:2, -1:2, -1:2], axes=[0, 0])
+    points = np.c_[px.ravel(), py.ravel(), pz.ravel()]
+
+    vor = Voronoi(points)
+
+    bz_facets = []
+    bz_ridges = []
+    bz_vertices = []
+
+    # for rid in vor.ridge_vertices:
+    #     if( np.all(np.array(rid) >= 0) ):
+    #         bz_ridges.append(vor.vertices[np.r_[rid, [rid[0]]]])
+    #         bz_facets.append(vor.vertices[rid])
+
+    for pid, rid in zip(vor.ridge_points, vor.ridge_vertices):
+        # WHY 13 ????
+        # The Voronoi ridges/facets are perpendicular to the lines drawn between the
+        # input points. The 14th input point is [0, 0, 0].
+        if pid[0] == 13 or pid[1] == 13:
+            bz_ridges.append(vor.vertices[np.r_[rid, [rid[0]]]])
+            bz_facets.append(vor.vertices[rid])
+            bz_vertices += rid
+
+    bz_vertices = list(set(bz_vertices))
+
+    return vor.vertices[bz_vertices], bz_ridges, bz_facets
