8a3822ccfb6a755aba83e2fb192a6caff81b5416
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-07-05 21:36:01 +0900

    Works!

--- script/draw_bz.py
+++ script/draw_bz.py
@@ -62,17 +62,32 @@ a3_z_input = dcc.Input(
 draw_bz_button = html.Button("Draw BZ", id="draw_bz_button", n_clicks=0)
 
 
-a1 = html.Div([a1_x_input, a1_y_input, a1_z_input])
-a2 = html.Div([a2_x_input, a2_y_input, a2_z_input])
-a3 = html.Div([a3_x_input, a3_y_input, a3_z_input])
+a1 = html.Div([html.P("a1"), a1_x_input, a1_y_input, a1_z_input])
+a2 = html.Div([html.P("a2"), a2_x_input, a2_y_input, a2_z_input])
+a3 = html.Div([html.P("a3"), a3_x_input, a3_y_input, a3_z_input])
 
 reciprocal_lattice = html.P(id="reciprocal")
+magnification = dcc.Input(
+    id="magnification", type="number", debounce=True, value=2, min=0.5
+)
 
-app.layout = html.Div([a1, a2, a3, draw_bz_button, reciprocal_lattice])
+
+app.layout = html.Div(
+    [
+        a1,
+        a2,
+        a3,
+        # html.Div([draw_bz_button, magnification]),
+        draw_bz_button,
+        reciprocal_lattice,
+        dcc.Graph(id="bz_graph"),
+    ]
+)
 
 
 @app.callback(
     Output("reciprocal", "children"),
+    Output("bz_graph", "figure"),
     State("a1_x", "value"),
     State("a1_y", "value"),
     State("a1_z", "value"),
@@ -82,6 +97,7 @@ app.layout = html.Div([a1, a2, a3, draw_bz_button, reciprocal_lattice])
     State("a3_x", "value"),
     State("a3_y", "value"),
     State("a3_z", "value"),
+    # State("magnification", "value"),
     Input("draw_bz_button", "n_clicks"),
 )
 def update_reciprocal(
@@ -94,6 +110,7 @@ def update_reciprocal(
     a3_x: float,
     a3_y: float,
     a3_z: float,
+    # mag: float,
     nclicks: int,
 ) -> str:
     cell = np.array(
@@ -107,6 +124,9 @@ def update_reciprocal(
     b1, b2, b3 = np.linalg.norm(icell, axis=1)
     # special_kpoints = get_special_points(cell)
     Verts_bz, Edges_bz, Facets_bz = get_bz_3d(icell)
+    # print(Verts_bz)
+    # print(np.max(Verts_bz))
+    range_value = np.max(np.abs(Verts_bz)) * 1.2
     icell_str = "{} {} {} \n{} {} {}\n{} {} {}\n".format(
         icell[0][0],
         icell[1][0],
@@ -118,10 +138,140 @@ def update_reciprocal(
         icell[1][2],
         icell[2][2],
     )
-    return icell_str
+    fig = go.Figure()
+    # plot the basis
+    basis_clrs = ["red", "green", "blue"]
+    basis_labs = [r"b<sub>{}</sub>".format(ii + 1) for ii in range(3)]
+    for ii, basis in enumerate(icell):
+        bx, by, bz = basis
+        fig.add_trace(
+            go.Scatter3d(
+                x=[0, bx],
+                y=[0, by],
+                z=[0, bz],
+                opacity=0.8,
+                hoverinfo="skip",
+                mode="lines+text",
+                line=dict(
+                    color=basis_clrs[ii],
+                    width=6,
+                ),
+                text=["", basis_labs[ii]],
+                textfont=dict(color=basis_clrs[ii], size=20),
+            )
+        )
 
+    bz_facet_clrs = [
+        "#636EFA",
+        "#EF553B",
+        "#00CC96",
+        "#AB63FA",
+        "#FFA15A",
+        "#19D3F3",
+        "#FF6692",
+        "#B6E880",
+        "#FF97FF",
+        "#FECB52",
+    ]
+    for shift in np.array(
+        [
+            [0, 0, 0],
+            # 2 * np.dot(special_kpoints["E"], icell),
+        ]
+    ):
+        sx, sy, sz = shift
+        # the vertices
+        fig.add_trace(
+            go.Scatter3d(
+                x=Verts_bz[:, 0] + sx,
+                y=Verts_bz[:, 1] + sy,
+                z=Verts_bz[:, 2] + sz,
+                opacity=0.8,
+                hoverinfo="skip",
+                mode="markers",
+                marker=dict(
+                    color="blue",
+                    size=6,
+                ),
+            )
+        )
 
-fig = go.Figure()
+        # the edges
+        for l in Edges_bz:
+            x, y, z = (l + shift).T
+            fig.add_trace(
+                go.Scatter3d(
+                    x=x,
+                    y=y,
+                    z=z,
+                    opacity=0.8,
+                    hoverinfo="skip",
+                    mode="lines",
+                    line=dict(
+                        color="black",
+                        width=5,
+                    ),
+                )
+            )
+
+        # the facets
+        edges_of_facets = list(np.sort(np.unique([len(ff) for ff in Facets_bz])))
+        for fi, ff in enumerate(Facets_bz):
+            face_clr_id = edges_of_facets.index(len(ff))
+            simplex_g = np.vstack([[0, 0, 0], ff])
+            tri = Delaunay(simplex_g)
+            for ii, xx in enumerate(tri.simplices):
+                # exclude the extra point when plotting faces
+                x, y, z = simplex_g[xx[xx != 0]].T
+                fig.add_trace(
+                    go.Mesh3d(
+                        x=x + sx,
+                        y=y + sy,
+                        z=z + sz,
+                        opacity=0.3,
+                        hoverinfo="skip",
+                        # color=bz_facet_clrs[face_clr_id],
+                        # color=bz_facet_clrs[fi % len(bz_facet_clrs)],
+                        color="gray",
+                        i=[0],
+                        j=[1],
+                        k=[2],
+                    )
+                )
+
+    ############################################################
+    camera = dict(
+        up=dict(x=0, y=0, z=1),
+        center=dict(x=0, y=0, z=0),
+        eye=dict(x=1.00, y=-1.20, z=0.00),
+    )
+    scene = dict(
+        camera=camera,
+        xaxis_showbackground=False,
+        yaxis_showbackground=False,
+        zaxis_showbackground=False,
+        xaxis_title="",
+        yaxis_title="",
+        zaxis_title="",
+        xaxis_range=[-range_value, range_value],
+        yaxis_range=[-range_value, range_value],
+        zaxis_range=[-range_value, range_value],
+        xaxis_tickvals=[],
+        yaxis_tickvals=[],
+        zaxis_tickvals=[],
+    )
+    # margin=dict(l=0, r=0, t=20, b=20)
+    fig.update_layout(
+        width=640,
+        height=640,
+        # margin=margin,
+        showlegend=False,
+        scene=scene,
+    )
+    # fig.update_scenes(camera_projection_type='orthographic')
+    # fix the ratio in the top left subplot to be a cube
+    fig.update_layout(scene_aspectmode="cube")
+    return icell_str, fig
 
 
 if __name__ == "__main__":
