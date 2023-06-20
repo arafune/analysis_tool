7bebe54b58cd14f7c38bdf6062c4ecd4ee60ab2b
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-06-20 11:41:04 +0900

    docs(sellmier.py): update docstring

--- pulselaser/sellmeier.py
+++ pulselaser/sellmeier.py
@@ -16,7 +16,8 @@ def three_term_sellmier(
     c3: float,
 ) -> float:
     r"""
-    :math:`n^2 -1 = \frac{B_1 \lambda^2}{\lambda^2 - c1} + \frac{B_1 \lambda^2}{\lambda^2 - C_2} + \frac{B_3 \lambda^2}{\lambda^2 - C_3}`
+    :math:`n^2 -1 = \frac{B_1 \lambda^2}{\lambda^2 - C_1} +
+    \frac{B_1 \lambda^2}{\lambda^2 - C_2} + \frac{B_3 \lambda^2}{\lambda^2 - C_3}`
 
 
     Parameters
@@ -123,7 +124,7 @@ def two_term_serllmier(
     lambda_micron: float, b1: float, c1: float, b2: float, c2: float
 ) -> float:
     r"""
-    :math:`n^2 -1 = \frac{B1 \lambda^2}{\lambda^2 - C1} + \frac{c \lambda^2}{\lambda^2 - d}`
+    :math:`n^2 -1 = \frac{B1 \lambda^2}{\lambda^2 - C_1} + \frac{c \lambda^2}{\lambda^2 - d}`
 
 
     Parameters
