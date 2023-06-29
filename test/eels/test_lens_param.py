d370e934408dc0bfa954765da3ecc980035c6044
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-06-29 19:25:53 +0900

    feat: Change the size of the test_eels_parameter.prm

--- test/eels/test_lens_param.py
+++ test/eels/test_lens_param.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-
 from pathlib import Path
 
-import eels.lens_param as lens_param
+from eels import lens_param
 
 
 class Test_LENS_parameter:
-    """Class for lens_param test"""
+    """Class for lens_param test."""
 
     def setup_method(self, method) -> None:
         datadir = Path(__file__).parent.resolve()
@@ -15,4 +13,4 @@ class Test_LENS_parameter:
         self.testprm = lens_param.load_els_lens_parameter(datafile)
 
     def test_lens_param(self):
-        assert len(self.testprm) == 6
+        assert len(self.testprm) == 8
--- test/qmas/test_sac.py
+++ test/qmas/test_sac.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Test for SAC class"""
+"""Test for SAC class."""
 
 import os
 
 import numpy as np
 
-import qmass.sac as sac
+from qmass import sac
 
 
 class Test_SAC_file:
-    """Class for qmas.sac.SACObject"""
+    """Class for qmas.sac.SACObject."""
 
     def setup_method(self, method):
         datadir = os.path.abspath(os.path.dirname(__file__)) + "/data/"
@@ -19,21 +18,21 @@ class Test_SAC_file:
 
     def test_parameters(self):
         """Check read parameters."""
-        assert 1 == self.sac.n_cyc
-        assert 1567616602 == self.sac.start_time
-        assert 2 == self.sac.n_dbc
-        assert 6412 == self.sac.cycle_length
-        assert 15 == self.sac.cyc_type
-        assert 218 == self.sac.hpos
-        assert 380 == self.sac.dpos
-        assert 8 == self.sac.df
-        assert 6 == self.sac.did
-        assert 0 == self.sac.sid
-        assert 0 == self.sac.firstmass
-        assert 50 == self.sac.scanwidth
-        assert 32 == self.sac.n_m  # number of measured value per pass
-        assert 1600 == self.sac.anz  # number pof points
-        assert -8 == self.sac.range
+        assert self.sac.n_cyc == 1
+        assert self.sac.start_time == 1567616602
+        assert self.sac.n_dbc == 2
+        assert self.sac.cycle_length == 6412
+        assert self.sac.cyc_type == 15
+        assert self.sac.hpos == 218
+        assert self.sac.dpos == 380
+        assert self.sac.df == 8
+        assert self.sac.did == 6
+        assert self.sac.sid == 0
+        assert self.sac.firstmass == 0
+        assert self.sac.scanwidth == 50
+        assert self.sac.n_m == 32  # number of measured value per pass
+        assert self.sac.anz == 1600  # number pof points
+        assert self.sac.range == -8
         #
         np.testing.assert_almost_equal(self.sac.current[0], 2.11444e-11)
         np.testing.assert_almost_equal(self.sac.current[-1], 3.35983e-11)
