6e6cca3fdb98423aa5b066ba118110f88bbd2066
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-07-03 19:06:49 +0900

    refactor(pes/__init__.py): Use TypeVar("A", NDArray[np.float_] | float)

--- pes/__init__.py
+++ pes/__init__.py
@@ -1,13 +1,16 @@
-# -*- coding: utf-8 -*-
 """Module to analyze and show ARPES data."""
-
 from __future__ import annotations
 
+from typing import TypeVar, reveal_type
+
 import numpy as np
+from numpy.typing import NDArray
+
+A = TypeVar("A", NDArray[np.float_], float)
 
 
-def delaytime_fs(mirror_movement_um: float) -> float:
-    """Return delaytime from the mirror movement
+def delaytime_fs(mirror_movement_um: A) -> A:
+    """Return delaytime from the mirror movement.
 
     Parameters
     ----------
@@ -22,8 +25,8 @@ def delaytime_fs(mirror_movement_um: float) -> float:
     return 3.335640951981521 * mirror_movement_um
 
 
-def position2delaytime(position_mm: float, center_position_mm: float) -> float:
-    """Return delay time from the mirror position
+def position2delaytime(position_mm: A, center_position_mm: float) -> A:
+    """Return delay time from the mirror position.
 
     Parameters
     ----------
@@ -39,8 +42,8 @@ def position2delaytime(position_mm: float, center_position_mm: float) -> float:
     return delaytime_fs(2 * (position_mm - center_position_mm) * 1000)
 
 
-def wavelength2eV(wavelength_nm: float) -> float:
-    """Return Energy of the light
+def wavelength2eV(wavelength_nm: A) -> A:  # noqa: N802
+    """Return Energy of the light.
 
     Parameters
     ----------
@@ -52,15 +55,14 @@ def wavelength2eV(wavelength_nm: float) -> float:
     float
         Photon enery in eV unit.
     """
-    planck_const: float = 6.62607015e-34  # J unit
-    planck_const_eV: float = 4.135667696e-15
+    planck_const_eV: float = 4.135667696e-15  # noqa: N806
     light_velocity: int = 299792458
 
     #  (h*c = 1.2398419840550368e-6)
     return planck_const_eV * light_velocity / (wavelength_nm * 1e-9)
 
 
-def parabolic_band_dispersion_k(k: float, e0: float, mass: float = 1.0) -> float:
+def parabolic_band_dispersion_k(k: A, e0: float, mass: float = 1.0) -> A:
     """Return the energy at the given k-point .
 
     Energy reference is the vacuum level.
@@ -80,12 +82,19 @@ def parabolic_band_dispersion_k(k: float, e0: float, mass: float = 1.0) -> float
     float
         Energy in eV unit measured from the vacuum level.
     """
-    return e0 + (1 / (0.512410908328 * np.sqrt(mass)) ** 2) * k**2
+    assert isinstance(k, np.ndarray | float)
+    assert isinstance(np.sqrt, float)
+    return e0 + (1 / (0.512410908328 * float(np.sqrt(mass))) ** 2) * k**2
+
+
+reveal_type(float(np.sqrt(3.0)))
 
 
 def parabolic_band_dispersion_angle(
-    theta_degree: float, e0: float, mass: float = 1.0
-) -> float:
+    theta_degree: A,
+    e0: float,
+    mass: float = 1.0,
+) -> A:
     """Return the energy at the given angle of emission (Free electron band).
 
     Energy reference is the vacuum level.
@@ -106,4 +115,9 @@ def parabolic_band_dispersion_angle(
     float
         Energy in eV unit measured from the vacuum level.
     """
+    assert isinstance(theta_degree, np.ndarray | float)
     return e0 * mass / (mass - np.sin(np.deg2rad(theta_degree)) ** 2)
+
+
+reveal_type(np.nan)
+reveal_type(np.sin(np.deg2rad(np.array([1, 2, 3]))))
