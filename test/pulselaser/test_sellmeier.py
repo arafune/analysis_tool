e4c8706eaffba5770f75c1c518b77187aac29a2d
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2023-12-04 16:25:18 +0900

    ✅  add tests

--- test/pulselaser/test_sellmeier.py
+++ test/pulselaser/test_sellmeier.py
@@ -1,22 +1,44 @@
 #! /usr/bin/env python3
 
-import numpy as np
-import pytest
-
 import pulselaser.sellmeier as sellmeier
 
 
-class Test_alhpaBBO:
+class TestBK7:
     def test_at_800nm(self) -> None:
-        assert sellmeier.alphaBBO(0.800) == (1.6448253125768209, 1.52590513720448)
+        assert sellmeier.bk7(0.80) == 1.5071655107493938
+
+
+class TestFusedSilica:
+    def test_at_800nm(self) -> None:
+        assert sellmeier.fused_silica(0.80) == 1.4533172570445876
+
+
+class TestCaF2:
+    def test_at_800nm(self) -> None:
+        assert sellmeier.caf2(0.80) == 1.4305724647561817
+
+
+class TestSF10:
+    def test_at_800nm(self) -> None:
+        assert sellmeier.sf10(0.80) == 1.696934373227597
+
+
+class TestQuartz:
+    def test_at_800nm(self) -> None:
+        assert sellmeier.quartz(0.80) == (1.5383355123424691, 1.5472301086112594)
+
+
+class TestAlhpaBBO:
+    def test_at_800nm(self) -> None:
+        assert sellmeier.alpha_bbo(0.800) == (1.6448253125768209, 1.52590513720448)
 
     def test_at_400nm(self) -> None:
-        assert sellmeier.alphaBBO(0.400) == (1.676336171290171, 1.5471238511499055)
+        assert sellmeier.alpha_bbo(0.400) == (1.676336171290171, 1.5471238511499055)
 
 
-class Test_betaBBO:
+class TestBetaBBO:
     def test_at_800nm(self) -> None:
-        assert sellmeier.betaBBO(0.800) == (1.660553524880645, 1.5444203018104292)
+        assert sellmeier.beta_bbo(0.800) == (1.660553524880645, 1.5444203018104292)
 
     def test_at_400nm(self) -> None:
-        assert sellmeier.betaBBO(0.400) == (1.6929832659808661, 1.5678876665187913)
+        assert sellmeier.beta_bbo(0.400) == (1.6929832659808661, 1.5678876665187913)
