95896e43e507fcf4bffc07772ac5cee27bff4119
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2024-04-08 10:51:59 +0900

    💬  Update type hints and correct linter warnings

--- eband/bz.py
+++ eband/bz.py
@@ -1,11 +1,15 @@
+"""Module for generation of the Brillouin zone."""
+
 import numpy as np
 from numpy.typing import NDArray
 from scipy.spatial import Voronoi
 
 
-def get_bz_3d(input_cell: NDArray[np.float_]):
-    """
-    Generate the Brillouin Zone of a given cell.
+def get_bz_3d(
+    input_cell: NDArray[np.float64],
+) -> tuple[NDArray[np.float64], list[NDArray[np.float64]], list[NDArray[np.float64]]]:
+    """Generate the Brillouin Zone of a given cell.
+
     The BZ is the Wigner-Seitz cell of the reciprocal lattice,
     which can be constructed by Voronoi decomposition
     to the reciprocal lattice.
@@ -14,12 +18,18 @@ def get_bz_3d(input_cell: NDArray[np.float_]):
 
     https://en.wikipedia.org/wiki/Wigner%E2%80%93Seitz_cell
     https://docs.scipy.org/doc/scipy/reference/tutorial/spatial.html#voronoi-diagrams
-    """
 
-    cell: NDArray[np.float_] = np.asarray(input_cell, dtype=float)
+    Parameters
+    ----------
+    input_cell
+        [TODO:description]
+
+    """
+    cell: NDArray[np.float64] = np.asarray(input_cell, dtype=float)
     assert cell.shape == (3, 3)
 
-    px, py, pz = np.tensordot(cell, np.mgrid[-1:2, -1:2, -1:2], axes=[0, 0])
+    points_ = np.tensordot(cell, np.mgrid[-1:2, -1:2, -1:2], axes=[0, 0])
+    px, py, pz = points_[0], points_[1], points_[2]
     points = np.c_[px.ravel(), py.ravel(), pz.ravel()]
 
     vor = Voronoi(points)
@@ -27,11 +37,11 @@ def get_bz_3d(input_cell: NDArray[np.float_]):
     bz_facets = []
     bz_ridges = []
     bz_vertices = []
-    for pid, rid in zip(vor.ridge_points, vor.ridge_vertices):
+    for pid, rid in zip(vor.ridge_points, vor.ridge_vertices, strict=True):
         # WHY 13 ????
         # The Voronoi ridges/facets are perpendicular to the lines drawn between the
         # input points. The 14th input point is [0, 0, 0].
-        if pid[0] == 13 or pid[1] == 13:
+        if pid[0] == 13 or pid[1] == 13:  # noqa: PLR2004
             bz_ridges.append(vor.vertices[np.r_[rid, [rid[0]]]])
             bz_facets.append(vor.vertices[rid])
             bz_vertices += rid
