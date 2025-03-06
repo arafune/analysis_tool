cef2f85bed917745e84e12dad3f71c9eca8e9990
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2025-03-07 08:21:36 +0900

    💡

--- pes/phi_correction.py
+++ pes/phi_correction.py
@@ -4,7 +4,10 @@ import numpy as np
 
 
 def based_on_pes_130(energy: float) -> float:
-    """Return the corrected phi value based on the energy."""
+    """Return the corrected phi value based on the energy.
+
+    Based on the results of PES_130 (sample: Cu(001))
+    """
     best_values = {
         "a": np.float64(0.021659247779521107),
         "b": np.float64(-0.19962481847388602),
